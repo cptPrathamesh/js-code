8b63f8b1d7c373dacc064791dba9b9dd1e8aefd8
cptPrathamesh <114138826+cptPrathamesh@users.noreply.github.com>
2025-06-30 06:48:33 +0000

    Understanding of variables.

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,26 @@
+const accountId=114453; //fixed value
+
+let accontEmail="abc@gmail.com";
+var accountPassword="12345";
+accountCity="Jaipur"
+let acccountState;
+
+
+// accountId=2 //not allowed
+console.log("accountId: "+accountId);
+
+accontEmail="hdfc@gmail.com";
+accountPassword="121221";
+accountCity="Thane";
+
+console.table([accontEmail,accountId,accountPassword,accountCity,acccountState]);
+
+// let and var mai difference
+/*
+    Prefered not to use var 
+
+*/
+
+
+
+
